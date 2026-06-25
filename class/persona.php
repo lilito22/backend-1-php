73f88bcb1b3eba8455e42d0e4d89db2dd5ba859f
Leidy Londono <leidylondono030@gmail.com>
2026-06-25 17:17:38 -0500

    Creación objeto persona

--- class/persona.php
+++ class/persona.php
@@ -4,6 +4,12 @@ class Persona
 {
     public function saludar()
     {
-        echo "Hola este es el primer ejercicio con programación Orientada a Objetos";
+        echo "Hola este es el primer ejercicio con programación Orientada a Objetos<br>";
+    }
+    
+    public function pintar()
+    {
+        echo "hola, pintemos un cuadro :)";
     }
 }
+
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once "../class/persona.php";
+
+$julieth = new Persona();
+
+$julieth->saludar();
+$julieth->pintar();
