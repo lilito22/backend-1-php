428541a3630c9927afd8e9459da38353b95b1220
Leidy Londono <leidylondono030@gmail.com>
2026-06-25 17:29:50 -0500

    Creación objeto animal

---
+++ class/animal.php
@@ -0,0 +1,14 @@
+<?php
+
+class perro 
+{
+    public function ladrar ()
+    {
+        echo "Woof Woof";
+    }
+
+    public function comer ()
+    {
+        echo "ñam, ñam, munch, munch";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -6,3 +6,12 @@ $julieth = new Persona();
 
 $julieth->saludar();
 $julieth->pintar();
+
+echo "<br><br>";
+
+require_once "../class/animal.php";
+$mascota = new perro();
+
+$mascota->ladrar();
+echo"<br>";
+$mascota->comer();
